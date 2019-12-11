d5a5cb3b3706eb99191abd89b421d8ac4a42eb1c
Wenner <wenner987@gmail.com>
2019-12-11 14:48:23 +0800

    move front end

--- src/main/webapp/js/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("div.nav-item").each(function(){
-        let menuWidth = $(this).find("div.nav-submenu").css("width");
-        if(menuWidth == undefined) return;
-        let val = "-" + String(Math.abs(pxHalf($(this).css("width")) - pxHalf(menuWidth))) + "px";
-        $(this).find("div.nav-submenu").css("left", val);
-    })
-})
